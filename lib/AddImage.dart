0783639bd712957a400b22c05f90977912663b4e
Sakshi Oswal <oswalsakshi17@gmail.com>
2020-05-24 12:40:37 +0530

    changed ui of add image

--- lib/AddImage.dart
+++ lib/AddImage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 class AddImage extends StatefulWidget {
   @override
@@ -10,69 +11,82 @@ class _AddImageState extends State<AddImage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color(0xff101010),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("Add your photo", style: TextStyle(
-                  fontFamily: 'MyriadPro-Regular',
-                  fontSize: 30,
-                  color: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 3,
+                child: Container(
 
-                ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text("Help your teammates know you are in Asana", style: TextStyle(
-                  fontFamily: 'MyriadPro-Regular',
-                  fontSize: 20,
-                  color: Colors.white,
+              Expanded(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.only(left: 30),
+                  alignment: Alignment.topLeft,
+                  child: Text("Add your photo", style: TextStyle(
+                    fontFamily: 'MyriadPro-Regular',
+                    fontSize: 25,
+                    color: Colors.white70,
 
-                ),
+
+                  ),
+
+                  ),
                 ),
               ),
-            ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.only(left: 30),
+                  alignment: Alignment.topLeft,
+                  child: Text("Help your teammates know you are in Asana", style: TextStyle(
+                    fontFamily: 'MyriadPro-Regular',
+                    fontSize: 14,
+                    color: Colors.white38,
 
-            Expanded(
-              flex: 5,
-              child: Container(
-                margin: EdgeInsets.all(20),
-                child: Center(
+                  ),
+                  ),
+                ),
+              ),
+
+              Expanded(
+                flex: 5,
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
                   child: CircleAvatar(
-                    backgroundColor: Colors.grey[800],
-                    radius: 100,
+                    backgroundColor: Colors.white10,
+                    radius: 90,
                     child: Icon(
                       Icons.camera_alt,
-                      size: 30,
-                      color: Colors.white70,
+                      size: 50,
+                      color: Colors.white38,
+
                     ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Text("Add photo", style: TextStyle(
-                fontFamily: 'MyriadPro-Regular',
-                fontSize: 20,
-                color: Colors.white,
+              Expanded(
+                flex: 1,
+                child: Text("Add photo", style: TextStyle(
+                  fontFamily: 'MyriadPro-Regular',
+                  fontSize: 17,
+                  color: Colors.white70,
 
+                ),
+                  textAlign: TextAlign.center,
+                ),
               ),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
+              Expanded(
+                flex: 3,
+                child: Container(
 
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
