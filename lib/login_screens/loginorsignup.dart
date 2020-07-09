5cbddff06a5e0403986c462b266246774be4de14
Parth15p <65505935+Parth15p@users.noreply.github.com>
2020-07-09 22:16:14 +0530

    gif

--- lib/login_screens/loginorsignup.dart
+++ lib/login_screens/loginorsignup.dart
@@ -13,20 +13,15 @@ class LoginSignUp extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
+      backgroundColor: const Color(0xff000000),
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
               image: const AssetImage('assets/bg1.gif'),
-              fit: BoxFit.cover,
-
             ),
           ),
           child: Column(
@@ -39,11 +34,12 @@ class LoginSignUp extends StatelessWidget {
                   margin: EdgeInsets.only(right: 10.0),
                   child: Align(
                     alignment: Alignment.topRight,
-                    child: Text('EventOut',
+                    child: Text(
+                      'EventOut',
                       style: TextStyle(
                         fontFamily: 'Helvetica',
                         fontSize: 20.889999389648438,
-                        color: const Color(0xffff5f7a),
+                        color: const Color(0xffe4a132),
                         height: 1.8321566023664637,
                       ),
                       textAlign: TextAlign.right,
@@ -65,42 +61,36 @@ class LoginSignUp extends StatelessWidget {
                         color: const Color(0xffffffff),
                         height: 1.2,
                       ),
-
                     ),
                   ),
                 ),
               ),
               Expanded(
                 flex: 12,
-                child: Container(
-
-                ),
+                child: Container(),
               ),
               Expanded(
                 flex: 4,
                 child: Container(
-                  margin: EdgeInsets.all(23) ,
+                  margin: EdgeInsets.all(23),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(29.5),
-                    color: const Color(0xffd80858),
+                    color: const Color(0xffe4a132),
                   ),
                   child: FlatButton(
-                    onPressed: (){
+                    onPressed: () {
                       navigateToGoogleorEmail(context);
                     },
                     child: Container(
-
                       child: Align(
                         alignment: Alignment.center,
                         child: Container(
-
                           child: Text(
                             'Sign Up',
                             style: TextStyle(
                               fontFamily: 'Helvetica',
                               fontSize: 30,
                               color: const Color(0xffffffff),
-
                             ),
                             textAlign: TextAlign.center,
                           ),
@@ -113,16 +103,15 @@ class LoginSignUp extends StatelessWidget {
               Expanded(
                 flex: 3,
                 child: FlatButton(
-                  onPressed: (){
+                  onPressed: () {
                     navigateToLogin(context);
-
                   },
                   child: Text(
-                    'Login In',
+                    'Log In',
                     style: TextStyle(
                       fontFamily: 'Helvetica',
                       fontSize: 30,
-                      color: const Color(0xff000000),
+                      color: const Color(0xffffffff),
                       height: 1.2,
                     ),
                     textAlign: TextAlign.center,
@@ -131,22 +120,16 @@ class LoginSignUp extends StatelessWidget {
               ),
               Expanded(
                 flex: 2,
-                child: Container(
-
-                ),
+                child: Container(),
               ),
-
             ],
-
           ),
         ),
       ),
     );
-
   }
-
-
 }
-class Data extends ChangeNotifier{
+
+class Data extends ChangeNotifier {
   String data = '123';
 }
